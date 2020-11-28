0a058056c91e88b620f3117a41fe4fce8be24e0f
Rixen <rixennoct@rambler.ru>
2020-11-28 17:18:59 +0300

    Initial commit

---
+++ src/main/java/ru/rxnnct/currencyconvertertrialproject/CurrencyConverterTrialProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.rxnnct.currencyconvertertrialproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterTrialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterTrialProjectApplication.class, args);
+	}
+
+}
