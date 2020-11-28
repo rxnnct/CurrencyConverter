70a7450fec55d59a19332a904c95356d9babb3a2
Rixen <rixennoct@rambler.ru>
2020-11-29 02:24:12 +0300

    Do saveXmlToDatabase in ApplicationRunner implementation.

--- src/main/java/ru/rxnnct/currencyconvertertrialproject/CurrencyConverterTrialProjectApplication.java
+++ src/main/java/ru/rxnnct/currencyconvertertrialproject/CurrencyConverterTrialProjectApplication.java
@@ -1,10 +1,14 @@
 package ru.rxnnct.currencyconvertertrialproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class CurrencyConverterTrialProjectApplication {
+	@Autowired
+	private ApplicationContext appContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConverterTrialProjectApplication.class, args);
