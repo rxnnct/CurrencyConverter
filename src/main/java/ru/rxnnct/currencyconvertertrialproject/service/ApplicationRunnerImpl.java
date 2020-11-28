b384c319cb5db9b59c515ae11721fe4db474d638
Rixen <rixennoct@rambler.ru>
2020-11-29 02:25:03 +0300

    Do saveXmlToDatabase in ApplicationRunner implementation.

---
+++ src/main/java/ru/rxnnct/currencyconvertertrialproject/service/ApplicationRunnerImpl.java
@@ -0,0 +1,18 @@
+package ru.rxnnct.currencyconvertertrialproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationRunnerImpl implements ApplicationRunner {
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        applicationContext.getBean(CbrXmlHandler.class).saveXmlToDatabase();
+    }
+}
